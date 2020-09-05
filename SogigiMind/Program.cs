abd38dd9d66c8a5fa57dfb9421c5be63b81b9fd4
azyobuzin <azyobuzin@users.sourceforge.jp>
2020-09-05 21:28:10 +0900

    Use JsonStreamLogger on Production

--- SogigiMind/Program.cs
+++ SogigiMind/Program.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +14,7 @@ namespace SogigiMind
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder => builder.AddJsonStream())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
--- SogigiMind/Startup.cs
+++ SogigiMind/Startup.cs
@@ -17,7 +17,7 @@ namespace SogigiMind
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            this.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
