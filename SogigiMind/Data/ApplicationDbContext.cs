568907227a75288753152ad7e83d1d1eb838181a
azyobuzin <azyobuzin@users.sourceforge.jp>
2020-09-27 03:21:18 +0900

    docker-compose で開発環境を作る

---
+++ SogigiMind/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SogigiMind.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+#pragma warning disable CS8618 // Null 非許容フィールドは初期化されていません。null 許容として宣言することを検討してください。
+        public ApplicationDbContext(DbContextOptions options)
+#pragma warning restore CS8618 // Null 非許容フィールドは初期化されていません。null 許容として宣言することを検討してください。
+            : base(options)
+        {
+        }
+
+        public DbSet<Blob> Blobs { get; set; }
+    }
+}
---
+++ SogigiMind/Data/Blob.cs
@@ -0,0 +1,23 @@
+﻿#nullable disable
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SogigiMind.Data
+{
+    public class Blob
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public byte[] Content { get; set; }
+
+        public string Etag { get; set; }
+
+        public DateTime? LastModified { get; set; }
+
+        [Column(TypeName = "jsonb")]
+        public string Metadata { get; set; }
+    }
+}
